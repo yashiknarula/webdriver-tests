0e2b2510a992cc24ba3ad3eaba279ce9f2fb29ba
yashik narula <38568688+yashiknarula@users.noreply.github.com>
2024-08-04 18:21:18 +0530

    Update LoginChrome.java

--- src/main/java/com/leaftaps/testcases/LoginChrome.java
+++ src/main/java/com/leaftaps/testcases/LoginChrome.java
@@ -31,7 +31,7 @@ public class LoginChrome {
 		//dc.setVersion("119.0");
 		dc.setPlatform(Platform.LINUX);
 
-		RemoteWebDriver driver = new RemoteWebDriver(new URL("http://4.240.76.74:4444/wd/hub"), dc);
+		RemoteWebDriver driver = new RemoteWebDriver(new URL("http://20.244.96.142:4444/wd/hub"), dc);
 
 		//ChromeDriver driver = new ChromeDriver();
 
